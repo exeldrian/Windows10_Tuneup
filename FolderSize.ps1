e180d32950dce2e6b801333d1cd74f262eae6044
exeldrian <exeldrian@gmail.com>
2020-06-08 12:46:39 -0400

    ya boi can't type

--- FolderSize.ps1
+++ FolderSize.ps1
@@ -6,7 +6,7 @@ Exeldrian 6/8/20
 Simple script to get a folder size. I will eventually add the ability to compare
 multiple folders, or at least print their sizes.
 
-When peeking into system directies (i.e. C:\Windows), run this as admininistrator.
+When peeking into system directies (i.e. C:\Windows), run this as administrator.
 
 #>
 
@@ -17,8 +17,10 @@ $sources = Read-Host('What folder do you want the size of? (full path): ')
 
 # Crunch the numbers
 Clear-Host
-Write-Host("Working on it...") # People get impatient... Big folders will make this script look like it stalled,,,
-$size = Get-ChildItem "$sources" -Recurse | Where-Object {$_ -notlike "AppData"} | Measure-Object -Property Length -Sum
+Write-Host("Working on it...") # People get impatient... Big folders will make this script look like it stalled...
+
+# Main scriptblock for the loop when I add in multiple path capability
+$size = Get-ChildItem "$sources" -Recurse | Measure-Object -Property Length -Sum
 $sizeGB = "{0:N1}" -f ($size.sum/1GB)
 
 #Print the results to screen
