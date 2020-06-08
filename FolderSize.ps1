ddced9922d88a7b1872002d0bdadb5df76830fc1
exeldrian <exeldrian@gmail.com>
2020-06-08 13:13:04 -0400

    makin' it purty

--- FolderSize.ps1
+++ FolderSize.ps1
@@ -13,7 +13,7 @@ When peeking into system directies (i.e. C:\Windows), run this as administrator.
 
 # Ask which folder        
 Clear-Host
-$sources = Read-Host('What folder do you want the size of? (full path): ')
+$sources = Read-Host('What folder do you want the size of? (full path)')
 
 # Crunch the numbers
 Clear-Host
@@ -25,4 +25,7 @@ $sizeGB = "{0:N1}" -f ($size.sum/1GB)
 
 #Print the results to screen
 Clear-Host # readability is important
-Write-Host("The total size of the directory you specified is $sizeGB GB.")
+Write-Host("I looked at:`r`n    $sources`r`n`r`nI saw a total of:`r`n    $sizeGB GB.`r`n`r`nPlease note that if you saw any errors while I was working
+then there are folders I don't have permission to see into.`r`n`r`n`r`n")
+
+Read-Host("Press enter to quit...             ")
